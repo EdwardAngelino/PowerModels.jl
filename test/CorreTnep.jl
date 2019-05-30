fdca9d91a4bb0f0409a89699231c3e2deea4e462
Edward Angelino <49659542+EdwardAngelino@users.noreply.github.com>
2019-05-30 07:54:52 -0500

    añade una corrida simple tnep

---
+++ test/CorreTnep.jl
@@ -0,0 +1,34 @@
+using PowerModels
+import InfrastructureModels
+import Memento
+
+# Suppress warnings during testing.
+Memento.setlevel!(Memento.getlogger(InfrastructureModels), "error")
+Memento.setlevel!(Memento.getlogger(PowerModels), "error")
+
+# for checking status codes
+import MathOptInterface
+const MOI = MathOptInterface
+
+import Cbc
+import Ipopt
+import SCS
+import Juniper
+
+import JuMP
+import JSON
+
+import LinearAlgebra
+using Test
+
+# default setup for solvers
+ipopt_solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, print_level=0)
+ipopt_ws_solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, mu_init=1e-4, print_level=0)
+
+cbc_solver = JuMP.with_optimizer(Cbc.Optimizer, logLevel=0)
+juniper_solver = JuMP.with_optimizer(Juniper.Optimizer, nl_solver=JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-4, print_level=0), mip_solver=cbc_solver, log_levels=[])
+scs_solver = JuMP.with_optimizer(SCS.Optimizer, max_iters=500000, acceleration_lookback=1, verbose=0)
+
+#include("common.jl")
+result = run_tnep("data/matpower/case5_tnep.m", ACPPowerModel, juniper_solver,setting = Dict("output" => Dict("branch_flows" => true)))
+#print("fin")
