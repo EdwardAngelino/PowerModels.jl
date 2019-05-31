f196b1ba6ab46a92908c303b9827908c51e6e055
Edward Angelino <eangeli@hotmail.com>
2019-05-31 11:10:17 -0500

    correropf y datos de ieee118

---
+++ test/correopf.jl
@@ -0,0 +1,39 @@
+
+using PowerModels
+using AmplNLWriter
+import InfrastructureModels
+import Memento
+
+# Suppress warnings during testing.
+Memento.setlevel!(Memento.getlogger(InfrastructureModels), "error")
+Memento.setlevel!(Memento.getlogger(PowerModels), "error")
+
+# for checking status codes
+import MathOptInterface
+const MOI = MathOptInterface
+
+import Cbc
+import Ipopt
+import SCS
+import Juniper
+
+import JuMP
+import JSON
+
+import LinearAlgebra
+using Test
+
+# default setup for solvers
+#ipopt_solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, print_level=0)
+#ipopt_solver = JuMP.with_optimizer(AmplNLWriter.Optimizer,"couenne", ["tol=1e-4", "print_level=0"])
+ipopt_solver = JuMP.with_optimizer(AmplNLWriter.Optimizer,"bonmin", ["tol=1e-4", "print_level=0"])
+
+ipopt_ws_solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, mu_init=1e-4, print_level=0)
+
+scs_solver = JuMP.with_optimizer(SCS.Optimizer, max_iters=500000, acceleration_lookback=1, verbose=0)
+
+data = PowerModels.parse_file("data/matpower/case6.m")
+PowerModels.print_summary(data)
+result = run_ac_opf(data, ipopt_solver,setting = Dict("output" => Dict("branch_flows" => true)))
+#result = run_opf(data, SOCWRPowerModel,  ipopt_solver,setting = Dict("output" => Dict("branch_flows" => true)))
+PowerModels.print_summary(result["solution"])
